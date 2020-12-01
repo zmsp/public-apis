f2df5b2649db2faf9e7d1e6bd03453170c8e489a
Zobair Shahadat <zobairshahadat@datamachines.io>
2020-12-01 02:27:17 -0500

    GH pages file added

---
+++ precache-manifest.140d6f8fa134dc581dc7af3401502fe5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "346e8fbe61608a1edbbc",
+    "url": "./static/css/main.490ecd8a.chunk.css"
+  },
+  {
+    "revision": "346e8fbe61608a1edbbc",
+    "url": "./static/js/main.bfffb710.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "dc2937bbcf6d05639eec",
+    "url": "./static/css/2.37c746a6.chunk.css"
+  },
+  {
+    "revision": "dc2937bbcf6d05639eec",
+    "url": "./static/js/2.f8e81670.chunk.js"
+  },
+  {
+    "revision": "abe4a318dfd1daed6d59f969027b0b53",
+    "url": "./static/media/material_bg.abe4a318.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "011201ab56695ce86ea2f190bce2670b",
+    "url": "./static/media/favicon.011201ab.ico"
+  },
+  {
+    "revision": "b54ba6b8b96b21675f5f8f183cfedd25",
+    "url": "./index.html"
+  }
+];
