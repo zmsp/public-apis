ff6a4a954b8374a0775712ec185d7e925842d935
Zobair Shahadat <zobairshahadat@datamachines.io>
2020-12-01 02:44:33 -0500

    static files updated

---
+++ precache-manifest.1bda216944a3da89b8bdb0ca3a16bda7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "3928eac81605d2046420",
+    "url": "/public-apis/static/css/main.490ecd8a.chunk.css"
+  },
+  {
+    "revision": "3928eac81605d2046420",
+    "url": "/public-apis/static/js/main.c8780458.chunk.js"
+  },
+  {
+    "revision": "b8781357d8abb4cd049d",
+    "url": "/public-apis/static/js/runtime~main.9003c575.js"
+  },
+  {
+    "revision": "05ac766d4bc9ec49f3e4",
+    "url": "/public-apis/static/css/2.410a9ca3.chunk.css"
+  },
+  {
+    "revision": "05ac766d4bc9ec49f3e4",
+    "url": "/public-apis/static/js/2.f8e81670.chunk.js"
+  },
+  {
+    "revision": "abe4a318dfd1daed6d59f969027b0b53",
+    "url": "/public-apis/static/media/material_bg.abe4a318.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/public-apis/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/public-apis/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/public-apis/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/public-apis/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/public-apis/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "011201ab56695ce86ea2f190bce2670b",
+    "url": "/public-apis/static/media/favicon.011201ab.ico"
+  },
+  {
+    "revision": "7d789e79ef79cf46985832b9a7381b43",
+    "url": "/public-apis/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.140d6f8fa134dc581dc7af3401502fe5.js"
+  "/public-apis/precache-manifest.1bda216944a3da89b8bdb0ca3a16bda7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/public-apis/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
